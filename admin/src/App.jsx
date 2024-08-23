3288081de4a33212dd64e881c022407b66037b8e
Георги Сталев <sttallev99@gmail.com>
2024-08-23 20:33:17 +0300

    Create needed component and pages and cofig routes

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,10 +1,25 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Navbar } from "./components/Navbar"
+import { Sidebar } from './components/Sidebar'
+import { List } from './pages/List'
+import { Orders } from './pages/Orders'
+import { Add } from './pages/Add'
+
 function App() {
 
   return (
-      <h1 class="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-
+    <BrowserRouter>
+      <Navbar />
+      <hr />
+      <div className='flex max-padd-container'>
+        <Sidebar />
+        <Routes>
+          <Route path='/add' element={<Add />}/>
+          <Route path='/add' element={<List/>}/>
+          <Route path='/add' element={<Orders/>}/>
+        </Routes>
+      </div>
+    </BrowserRouter>
   )
 }
 
