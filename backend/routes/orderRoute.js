d9ce575930129528556851e7a76a4a8fd6b36071
Георги Сталев <sttallev99@gmail.com>
2024-08-26 02:52:31 +0300

    fix a bug

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -5,6 +5,6 @@ import { placeOrder, verifyOrder } from "../controllers/orderController.js";
 const orderRouter = express.Router();
 
 orderRouter.post("/place", authMiddleware, placeOrder);
-orderRouter.post('/verify', authMiddleware, verifyOrder);
+orderRouter.post('/verify', verifyOrder);
 
 export default orderRouter;
