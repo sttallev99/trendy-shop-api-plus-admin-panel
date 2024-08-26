515a0795e35de02cea0577ddc5a70eea18913724
Георги Сталев <sttallev99@gmail.com>
2024-08-26 13:15:43 +0300

    add list all orders route

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -77,5 +77,16 @@ const userOrders = async(req, res) => {
     }
 }
 
+//listing orders in admin panel
+const listOrders = async(req, res) => {
+    try {
+        const orders = await orderModel.find({});
+        res.json({success: true, data: orders});
+    } catch (error) {
+        console.log(error)
+        res.json({success: false, message: "Error"});
+    }
+}
 
-export {placeOrder, verifyOrder, userOrders }
+
+export {placeOrder, verifyOrder, userOrders, listOrders }
